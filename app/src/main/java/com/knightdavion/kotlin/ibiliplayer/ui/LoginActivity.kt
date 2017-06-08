64dbe861d0eef2fa050d25b349635924b9dec17e
Administrator <shiwei.bsw@gmail.com>
2017-06-08 14:15:15 +0800

    导入图片

--- app/src/main/java/com/knightdavion/kotlin/ibiliplayer/ui/LoginActivity.kt
+++ app/src/main/java/com/knightdavion/kotlin/ibiliplayer/ui/LoginActivity.kt
@@ -2,12 +2,20 @@ package com.knightdavion.kotlin.ibiliplayer.ui
 
 import android.os.Bundle
 import com.knightdavion.kotlin.ibiliplayer.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        initToolBar();
         isLogin = true
+    }
+
+    private fun initToolBar() {
+        toolbar.setNavigationIcon(R.drawable.ic_action_back)
+        toolbar.title = getString(R.string.login)
+        toolbar.setNavigationOnClickListener { finish() }
     }
 }
